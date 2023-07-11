75887dfd366a4d423b7217b51a848a0b327e8dfc
Akenten <ars.alex19@yandex.ru>
2023-07-11 12:21:56 +0300

    added navBar and few buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-//import 'package:flutter/services.dart';
+import 'main_page.dart';
 
 void main() {
   //SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
@@ -16,178 +15,36 @@ class ReUpApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(48),
-          child: AppBar(),
-        ),
-        body: MainPage(),
-      ),
-    );
-  }
-}
-
-final controller = PageController(
-  initialPage: 0,
-);
-
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: SizedBox(
-      //height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Container(
-            height: 734, //678
-            child: PageView(
-              controller: controller,
-
-              children: [
-                Promo(
-                    data: PromoData(
-                        Image(image: AssetImage('assets/images/reup_img1.jpg')),
-                        Image(image: AssetImage('assets/images/reup_img2.jpg')),
-                        'новый бренд',
-                        'Befree',
-                        'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей ')),
-                Promo(
-                    data: PromoData(
-                        Image(image: AssetImage('assets/images/reup_img1.jpg')),
-                        Image(image: AssetImage('assets/images/reup_img2.jpg')),
-                        '1',
-                        '1',
-                        '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 ')),
-              ],
-
-              //верстка дальше
-            ),
+          appBar: const PreferredSize(
+            preferredSize: Size.fromHeight(48),
+            child: AppBar(),
           ),
-          Text('data'),
-        ],
-      ),
-    ));
-  }
-}
-
-class PromoData {
-  final Image img1;
-  final Image img2;
-  final String title;
-  final String subtitle;
-  final String text;
-
-  PromoData(this.img1, this.img2, this.title, this.subtitle, this.text);
-}
-
-class Promo extends StatelessWidget {
-  final PromoData data;
-  const Promo({super.key, required this.data});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      data.img1,
-      data.img2,
-      Container(
-          //height: 284, //276 284
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          decoration: const BoxDecoration(color: Colors.white),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Text(
-                  data.title,
-                  style: GoogleFonts.delaGothicOne(
-                    color: Colors.black,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w400,
-                    //eight: 35.20,
-                    letterSpacing: 1.28,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                // линия
-                width: double.infinity,
-                decoration: const ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      width: 0.50,
-                      //strokeAlign:
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                //height: 161,
-                child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(data.subtitle,
-                          style: GoogleFonts.delaGothicOne(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                            letterSpacing: 0.80,
-                          )),
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          data.text,
-                          style: const TextStyle(
-                            fontFamily: 'Gilroy',
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                            letterSpacing: 0.98,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      Align(
-                        //more button
-                        alignment: Alignment.centerLeft,
-                        child: IconButton(
-                          onPressed: (() {
-                            log('more');
-                          }),
-                          icon: SvgPicture.asset(
-                              'assets/icons/reup_icon_more.svg'),
-                          iconSize: 36,
-                        ),
-                      )
-                      // Align(
-                      //   alignment: Alignment.centerLeft,
-                      //   child: IconButton(
-                      //     onPressed: (() {
-                      //       log('more');
-                      //     }),
-                      //     icon: SvgPicture.asset('assets/icons/Button.svg'),
-                      //     iconSize: 40,
-                      //   ),
-                      // )
-                    ]),
-              ),
-            ],
-          ))
-    ]);
+          body: MainPage(),
+          backgroundColor: Colors.white,
+          bottomNavigationBar: SizedBox(
+            height: 56,
+            child: NavigationBar(backgroundColor: Colors.white, destinations: [
+              NavigationDestination(
+                  icon: SvgPicture.asset('assets/icons/reup_icon.svg'),
+                  label: ''),
+              NavigationDestination(
+                  icon: SvgPicture.asset('assets/icons/reup_icon_list.svg'),
+                  label: ''),
+              NavigationDestination(
+                  icon: SvgPicture.asset('assets/icons/reup_icon_cart.svg'),
+                  label: ''),
+              NavigationDestination(
+                  icon: SvgPicture.asset('assets/icons/reup_icon_favorite.svg'),
+                  label: ''),
+              NavigationDestination(
+                  icon: SvgPicture.asset('assets/icons/reup_icon_profile.svg'),
+                  label: ''),
+            ]),
+          )),
+    );
   }
 }
 
@@ -214,8 +71,8 @@ class AppBar extends StatelessWidget {
                   letterSpacing: 0.96,
                 )),
             IconButton(
-                icon: const ImageIcon(AssetImage(
-                    'assets/icons/reup_search.png')), //вставить реаповскую иконку
+                icon:
+                    const ImageIcon(AssetImage('assets/icons/reup_search.png')),
                 iconSize: 32,
                 onPressed: (() {
                   log('message');
---
+++ lib/main_page.dart
@@ -0,0 +1,198 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: SizedBox(
+      //height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: [
+          Container(
+            height: 734, //678
+            child: PageView(
+              controller: controller,
+
+              children: [
+                Promo(
+                    data: PromoData(
+                        const Image(
+                            image: AssetImage('assets/images/reup_img1.jpg')),
+                        const Image(
+                            image: AssetImage('assets/images/reup_img2.jpg')),
+                        'новый бренд',
+                        'Befree',
+                        'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей ')),
+                Promo(
+                    data: PromoData(
+                        const Image(
+                            image: AssetImage('assets/images/reup_img1.jpg')),
+                        const Image(
+                            image: AssetImage('assets/images/reup_img2.jpg')),
+                        '1',
+                        '1',
+                        '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 ')),
+              ],
+
+              //верстка дальше
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: OutlinedButton(
+                          onPressed: (() {
+                            log('more');
+                          }),
+                          style: ButtonStyle(),
+                          child: const Text('верхняя одежда',
+                              style: TextStyle(
+                                fontFamily: 'Gilroy',
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 1.12,
+                              )))),
+                  Expanded(
+                      flex: 1,
+                      child: OutlinedButton(
+                          onPressed: (() {
+                            log('more');
+                          }),
+                          style: ButtonStyle(),
+                          child: const Text('обувь',
+                              style: TextStyle(
+                                fontFamily: 'Gilroy',
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 1.12,
+                              ))))
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+final controller = PageController(
+  initialPage: 0,
+);
+
+class PromoData {
+  final Image img1;
+  final Image img2;
+  final String title;
+  final String subtitle;
+  final String text;
+
+  PromoData(this.img1, this.img2, this.title, this.subtitle, this.text);
+}
+
+class Promo extends StatelessWidget {
+  final PromoData data;
+  const Promo({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      data.img1,
+      data.img2,
+      Container(
+          //height: 284, //276 284
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          decoration: const BoxDecoration(color: Colors.white),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  data.title,
+                  style: GoogleFonts.delaGothicOne(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                    //height: 35.20,
+                    letterSpacing: 1.28,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                // линия
+                width: double.infinity,
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 0.50,
+                      //strokeAlign:
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                //height: 161,
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(data.subtitle,
+                          style: GoogleFonts.delaGothicOne(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0.80,
+                          )),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          data.text,
+                          style: const TextStyle(
+                            fontFamily: 'Gilroy',
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 0.98,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Align(
+                        //more button
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
+                          onPressed: (() {
+                            log('more');
+                          }),
+                          icon: SvgPicture.asset(
+                              'assets/icons/reup_icon_more.svg'),
+                          iconSize: 36,
+                        ),
+                      )
+                    ]),
+              ),
+            ],
+          ))
+    ]);
+  }
+}
