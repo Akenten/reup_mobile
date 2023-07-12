69d58c5baafbb932abad5e1fdd614001ff783ea3
Akenten <ars.alex19@yandex.ru>
2023-07-12 18:36:22 +0300

    added buttons

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,79 +10,251 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: SizedBox(
-      //height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Container(
-            height: 734, //678
-            child: PageView(
-              controller: controller,
+        child: Column(
+      children: [
+        Container(
+          height: 734, //678
+          child: PageView(
+            controller: controller,
 
-              children: [
-                Promo(
-                    data: PromoData(
-                        const Image(
-                            image: AssetImage('assets/images/reup_img1.jpg')),
-                        const Image(
-                            image: AssetImage('assets/images/reup_img2.jpg')),
-                        'новый бренд',
-                        'Befree',
-                        'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей ')),
-                Promo(
-                    data: PromoData(
-                        const Image(
-                            image: AssetImage('assets/images/reup_img1.jpg')),
-                        const Image(
-                            image: AssetImage('assets/images/reup_img2.jpg')),
-                        '1',
-                        '1',
-                        '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 ')),
-              ],
-
-              //верстка дальше
-            ),
-          ),
-          Column(
             children: [
-              Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: OutlinedButton(
-                          onPressed: (() {
-                            log('more');
-                          }),
-                          style: ButtonStyle(),
-                          child: const Text('верхняя одежда',
-                              style: TextStyle(
-                                fontFamily: 'Gilroy',
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                                letterSpacing: 1.12,
-                              )))),
-                  Expanded(
-                      flex: 1,
-                      child: OutlinedButton(
-                          onPressed: (() {
-                            log('more');
-                          }),
-                          style: ButtonStyle(),
-                          child: const Text('обувь',
-                              style: TextStyle(
-                                fontFamily: 'Gilroy',
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                                letterSpacing: 1.12,
-                              ))))
-                ],
-              ),
+              Promo(
+                  data: PromoData(
+                      const Image(
+                          image: AssetImage('assets/images/reup_img1.jpg')),
+                      const Image(
+                          image: AssetImage('assets/images/reup_img2.jpg')),
+                      'новый бренд',
+                      'Befree',
+                      'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей ')),
+              Promo(
+                  data: PromoData(
+                      const Image(
+                          image: AssetImage('assets/images/reup_img1.jpg')),
+                      const Image(
+                          image: AssetImage('assets/images/reup_img2.jpg')),
+                      '1',
+                      '1',
+                      '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 ')),
             ],
-          )
-        ],
-      ),
+
+            //верстка дальше
+          ),
+        ),
+        Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerRight,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(right: 16)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('верхняя одежда',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            )))),
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('обувь',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            ))))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerLeft,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(left: 89)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('сумки',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            )))),
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerRight,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(right: 32)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('рубашки',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            ))))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerLeft,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(left: 40)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('платья',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            )))),
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerRight,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(right: 100)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('костюмы',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            ))))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.center,
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('штаны',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            )))),
+                Expanded(
+                    flex: 1,
+                    child: OutlinedButton(
+                        onPressed: (() {
+                          log('more');
+                        }),
+                        style: ButtonStyle(
+                            alignment: Alignment.centerRight,
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                    EdgeInsets.only(right: 16)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(
+                                        color: Colors.black, width: 1)))),
+                        child: const Text('аксессуары',
+                            style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.12,
+                            ))))
+              ],
+            ),
+          ],
+        )
+      ],
     ));
   }
 }
