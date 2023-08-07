cd5579ef2b0c313bd18a89cad895af50ae61279e
Akenten <ars.alex19@yandex.ru>
2023-08-07 19:36:26 +0300

    collection view fixes

--- lib/collection.dart
+++ lib/collection.dart
@@ -26,31 +26,43 @@ class _collectionState extends State<collection> {
         border: Border.all(color: Colors.black, width: 0.5),
         color: Colors.white,
       ),
-      child: Stack(
-        children: [
-          Container(
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(color: Colors.black, width: 0.5))),
-              child: widget.data.img),
-          Positioned(
-            top: 181,
-            left: 8,
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                widget.data.collectionName,
-                style: CustomTextStyle.boldTextStyle,
-              ),
+      child: Column(children: [
+        Container(
+          width: 179,
+          height: 173,
+          decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black, width: 0.5)),
+          ),
+          child: FittedBox(
+            child: widget.data.img,
+            fit: BoxFit.fill,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Text(
+                  widget.data.collectionName,
+                  style: CustomTextStyle.boldTextStyle,
+                  maxLines: 1,
+                ))),
+        const SizedBox(
+          height: 8,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 8),
+            child: Text(
+              widget.data.Brand,
+              style: CustomTextStyle.promoTextStyle,
+              maxLines: 1,
             ),
           ),
-          Positioned(
-              left: 8,
-              bottom: 8,
-              child: Text(widget.data.Brand,
-                  style: CustomTextStyle.promoTextStyle))
-        ],
-      ),
+        )
+      ]),
     );
   }
 }
